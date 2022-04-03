842f862a65e47e6db03a603ef5510263bc3265ef
okkvlt <occult.aiwass@gmail.com>
2022-04-03 01:42:25 -0300

    Ordenador de Vetor (ordem crescente)

---
+++ sort.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+    Ordena o vetor em ordem crescente.
+*/
+
+void fill(int *v, int size){
+    for(int i=0; i<size; i++){
+        v[i]=rand() % 100;
+    }
+}
+
+void print_v(int *v, int size){
+    for(int i=0; i<size; i++){
+        printf("%d ", v[i]);
+    }
+    puts("");
+}
+
+void swap(int *v, int i, int j){
+    int temp = v[i];
+    v[i] = v[j];
+    v[j] = temp;
+}
+
+void sort_v(int *v, int size){
+    int i,j;
+    for(i=0; i<size; i++){
+        for(j=i+1; j<size; j++){
+            if(v[i]>v[j]){
+                swap(v,i,j);
+            }
+        }
+    }
+}
+
+int main(){
+    int v[10];
+
+    fill(v,10);
+    print_v(v,10);
+    sort_v(v,10);
+    print_v(v,10);
+
+    return 0;
+}
